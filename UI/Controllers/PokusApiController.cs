a9692f8efe17ed04da0663ee12e8d5b3aa9c673a
Jiri Theimer <jiri.theimer@gmail.com>
2021-01-14 00:27:23 +0100

    a

--- UI/Controllers/PokusApiController.cs
+++ UI/Controllers/PokusApiController.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using Newtonsoft.Json;
+
 using UI.Models;
 
 namespace UI.Controllers
@@ -33,7 +35,7 @@ namespace UI.Controllers
 
             return await response.Content.ReadAsStringAsync();
         }
-
+        
         public async Task<string> Simple2(string city)
         {
             var url = "http://api.weatherapi.com/v1/current.json?key=fc4dba410de847eaa28202954211301&q=" + city;
@@ -96,5 +98,70 @@ namespace UI.Controllers
             return c.current.temp_c;
         }
 
+        public async Task<float> Vitr(string city)
+        {
+            var url = "http://api.weatherapi.com/v1/current.json?key=fc4dba410de847eaa28202954211301&q=" + city;
+
+            var httpclient = _httpclientfactory.CreateClient();
+            
+
+            var response = await httpclient.GetAsync(url);
+
+
+            var strJson = await response.Content.ReadAsStringAsync();
+
+            var c = JsonConvert.DeserializeObject<Pocasi>(strJson);
+
+            return c.current.wind_kph;
+        }
+
+        public async Task<string> Fidoo1()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://api.fidoo.com/v2/user/get-user"))
+                {
+                    request.Headers.TryAddWithoutValidation("Accept", "application/json");
+                    request.Headers.TryAddWithoutValidation("X-Api-Key", "paklic1");
+
+                    request.Content = new StringContent("{  \n   \"userId\": \"d046b326-3d6c-4dc3-852f-3999ac3f0632\"  \n }");
+
+                    
+                    request.Content.Headers.ContentType = new MediaTypeWithQualityHeaderValue("application/json");
+
+                   
+                    var response = await httpClient.SendAsync(request);
+
+                    return await response.Content.ReadAsStringAsync();
+                }
+            }
+        }
+
+        
+        public async Task<string> FiDoo2()
+        {
+            
+            var httpclient = _httpclientfactory.CreateClient();
+
+            using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://api.fidoo.com/v2/user/get-user"))
+            {
+                request.Headers.TryAddWithoutValidation("Accept", "application/json");
+                request.Headers.TryAddWithoutValidation("X-Api-Key", "paklic1");
+
+                request.Content = new StringContent("{  \n   \"userId\": \"d046b326-3d6c-4dc3-852f-3999ac3f0632\"  \n }");
+
+
+                request.Content.Headers.ContentType = new MediaTypeWithQualityHeaderValue("application/json");
+
+
+                var response = await httpclient.SendAsync(request);
+
+                return await response.Content.ReadAsStringAsync();
+            }
+
+        }
+
     }
+
+
 }
