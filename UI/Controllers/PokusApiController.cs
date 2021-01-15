f3c7ac4dfe3b7dd6d8adc5722af4b6391441a7bc
Jiri Theimer <jiri.theimer@gmail.com>
2021-01-15 23:05:33 +0100

    a

--- UI/Controllers/PokusApiController.cs
+++ UI/Controllers/PokusApiController.cs
@@ -76,8 +76,8 @@ namespace UI.Controllers
 
 
             var strJson = await response.Content.ReadAsStringAsync();
-
-            return Newtonsoft.Json.JsonConvert.DeserializeObject<Pocasi>(strJson);
+            
+            return JsonConvert.DeserializeObject<Pocasi>(strJson);
         }
 
 
@@ -140,16 +140,79 @@ namespace UI.Controllers
         
         public async Task<string> FiDoo2()
         {
+            //indický youtube návod: https://www.youtube.com/watch?v=bAXZx0zOeCU&ab_channel=RahulNath
+            //využito z: https://curl.olsh.me/
+            //návod pro práci s HttpClient: https://gist.github.com/dfch/7b338046d5e63e3b3106
+            //článek o používání: SYSTEM.NET.HTTP.JSON: https://www.stevejgordon.co.uk/sending-and-receiving-json-using-httpclient-with-system-net-http-json
+
+            //článek k request (POST) v rámci httpclient: https://stackoverflow.com/questions/60420786/using-c-sharp-httpclient-how-to-post-a-string-httpcontent-to-asp-net-core-web-a
+            //swagger pro FiDoo: https://api.fidoo.com/v2/#!/public45settings45api45v452/getCostCentersUsingPOST_1
+
+            //první FIDOO api klíč: pak1jgnBUswEVDGTkdT86v21XKMT0UYZSK0rYAq8Ecb1baMI5JskcAIZC32sMbw6
+            //jeho zamaskovaná hodnota: pak1jgnBUswEVDGTkdT
+
+            var httpclient = _httpclientfactory.CreateClient();
+
+            using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://api.fidoo.com/v2/user/get-user-by-email"))
+            {
+                request.Headers.TryAddWithoutValidation("Accept", "application/json");
+                request.Headers.TryAddWithoutValidation("X-Api-Key", "pak1jgnBUswEVDGTkdT86v21XKMT0UYZSK0rYAq8Ecb1baMI5JskcAIZC32sMbw6");
+
+                var s= JsonConvert.SerializeObject(new UI.Models.Fidoo.UserByEmail() { email = "jiri.theimer@marktime.cz" });
+                request.Content = new StringContent(s);
+
+                //request.Content = new StringContent("{  \n   \"email\": \"jiri.theimer@marktime.cz\"  \n }");
+
+               
+               
+                request.Content.Headers.ContentType = new MediaTypeWithQualityHeaderValue("application/json");
+
+
+                var response = await httpclient.SendAsync(request);
+
+                return await response.Content.ReadAsStringAsync();
+            }
+
+        }
+
+        public async Task<string> FiDoo3()
+        {
             
             var httpclient = _httpclientfactory.CreateClient();
 
-            using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://api.fidoo.com/v2/user/get-user"))
+            using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://api.fidoo.com/v2/settings/get-cost-centers"))
             {
                 request.Headers.TryAddWithoutValidation("Accept", "application/json");
-                request.Headers.TryAddWithoutValidation("X-Api-Key", "paklic1");
+                request.Headers.TryAddWithoutValidation("X-Api-Key", "pak1jgnBUswEVDGTkdT86v21XKMT0UYZSK0rYAq8Ecb1baMI5JskcAIZC32sMbw6");
 
-                request.Content = new StringContent("{  \n   \"userId\": \"d046b326-3d6c-4dc3-852f-3999ac3f0632\"  \n }");
-               
+                var s = JsonConvert.SerializeObject(new UI.Models.Fidoo.QueryRequestRoot());
+                request.Content = new StringContent(s);
+
+                request.Content.Headers.ContentType = new MediaTypeWithQualityHeaderValue("application/json");
+
+
+                var response = await httpclient.SendAsync(request);
+
+                return await response.Content.ReadAsStringAsync();
+            }
+
+        }
+
+        public async Task<string> FiDoo4()
+        {
+
+            var httpclient = _httpclientfactory.CreateClient();
+
+            using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://api.fidoo.com/v2/expense/get-expenses"))
+            {
+                request.Headers.TryAddWithoutValidation("Accept", "application/json");
+                request.Headers.TryAddWithoutValidation("X-Api-Key", "pak1jgnBUswEVDGTkdT86v21XKMT0UYZSK0rYAq8Ecb1baMI5JskcAIZC32sMbw6");
+
+                var d1 = new DateTime(2021, 1, 1).ToLocalTime();
+                var d2 = d1.AddYears(1).ToLocalTime();
+                var s = JsonConvert.SerializeObject(new UI.Models.Fidoo.QueryRequestExpenses() {lastModifyFrom=d1,from=d1,to=d2,limit=1000 });
+                request.Content = new StringContent(s);
+
                 request.Content.Headers.ContentType = new MediaTypeWithQualityHeaderValue("application/json");
 
 
---
+++ UI/Models/Pokus/Fidoo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UI.Models.Fidoo
+{
+
+    //Volání uživatelského účtu podle e-mail adresy
+    public class UserByEmail
+    {
+        public string email { get; set; }
+    }
+
+
+    //vstupní filtr pro seznam středisek a projektů
+    public class QueryRequestRoot
+    {
+        public Queryrequest queryRequest { get; set; }
+    }
+
+    public class Queryrequest
+    {
+        public int limit { get; set; } = 100;
+        public int offset { get; set; }
+        public Sort[] sort { get; set; }
+    }
+
+    public class Sort
+    {
+        public bool ascendant { get; set; }
+        public string property { get; set; }
+    }
+
+
+    //vstupní filtr pro seznam výdajů
+
+    public class QueryRequestExpenses
+    {
+        public DateTime from { get; set; }
+        public DateTime to { get; set; }
+        public DateTime lastModifyFrom { get; set; }
+        public int limit { get; set; }
+    }
+
+
+    
+
+
+
+
+}
