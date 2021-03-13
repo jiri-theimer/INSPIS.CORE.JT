07ed0cee5ac7d66452b28d700ac658b5924e7595
Jiri Theimer <jiri.theimer@gmail.com>
2021-03-13 22:00:38 +0100

    a

--- BL/f32FilledValueBL.cs
+++ BL/f32FilledValueBL.cs
@@ -55,7 +55,7 @@ namespace BL
                 s += " AND a.f21ID=@f21id";
                 p.AddInt("f21id", f21id);
             }
-            BO.f32FilledValue c = _db.Load<BO.f32FilledValue>(s,p);
+            BO.f32FilledValue c = _db.Load<BO.f32FilledValue>(s,p.getDynamicDapperPars());
             if (c !=null && c.f33ID > 0)
             {
                 c.Value = GetF33Value(c.f33ID);
